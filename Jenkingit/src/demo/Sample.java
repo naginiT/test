87b3e9a292d5a69c6c97d057511c8059362859f4
cubic <cubic@192.168.0.39>
2018-09-20 14:57:15 +0530

    Sample

---
+++ Jenkingit/src/demo/Sample.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\appselenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/upload/");
+		driver.manage().window().maximize();
+
+	}
+
+}
